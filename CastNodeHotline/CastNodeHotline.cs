0e58cd804d94d568ee66ca67e3f0771891a95f13
art0007i <art0007i@gmail.com>
2022-05-04 14:32:36 +0200

    Add project files.

---
+++ CastNodeHotline/CastNodeHotline.cs
@@ -0,0 +1,37 @@
+using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+
+namespace CastNodeHotline
+{
+    public class CastNodeHotline : NeosMod
+    {
+        public override string Name => "CastNodeHotline";
+        public override string Author => "art0007i";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/art0007i/CastNodeHotline/";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("me.art0007i.CastNodeHotline");
+
+            var method = typeof(LogixNode).GetMethod("Cleanup", BindingFlags.NonPublic | BindingFlags.Instance);
+            var patchMethod = typeof(CastNodeHotlinePatch).GetMethod("Prefix");
+            harmony.Patch(method, new HarmonyMethod(patchMethod));
+        }
+
+        class CastNodeHotlinePatch
+        {
+            public static bool Prefix()
+            {
+                Warn("Woah dude watch out the patch is patching !");
+                return false;
+            }
+        }
+    }
+}
